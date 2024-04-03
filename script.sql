b76d9aa1b7a09c93a9f1596baae08a89eeb223d6
Santiago Santafe <andressasi@unisabana.edu.co>
2024-04-03 10:46:33 -0500

    Correcciones de mayúsculas y traducción al español en consultas SQL

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-select
+SELECT
     c.nombre,
     c.documento,
     c.telefono,
@@ -7,19 +7,22 @@ select
     c.vetado,
     ir.id as ir_id,
     ir.fechainiciorenta as ir_fechainiciorenta,
-    ir.fechafinrenta as_fechafinrenta,
+    ir.fechafinrenta as ir_fechafinrenta,
     i.id as i_id,
     i.nombre as i_nombre,
     i.descripcion as i_descripcion,
     i.fechalanzamiento as i_fechalanzamiento,
     i.tarifaxdia as i_tarifaxdia,
-    i.formatorenta as i_formatorenta ,
-    i.genero as i_genero,        
+    i.formatorenta as i_formatorenta,
+    i.genero as i_genero,
     ti.id as ti_id,
     ti.descripcion as ti_descripcion
-    FROM VI_CLIENTES as c 
-    left join VI_ITEMRENTADO as ir on c.documento=ir.CLIENTES_documento 
-    left join VI_ITEMS as i on ir.ITEMS_id=i.id 
-    left join VI_TIPOITEM as ti on i.TIPOITEM_id=ti.id 
+FROM VI_CLIENTES as c
+LEFT JOIN VI_ITEMRENTADO as ir ON c.documento = ir.CLIENTES_documento
+LEFT JOIN VI_ITEMS as i ON ir.ITEMS_id = i.id
+LEFT JOIN VI_TIPOITEM as ti ON i.TIPOITEM_id = ti.id;
 
-          SELECT id as i_id, nombre as i_nombre, descripcion as i_descripcion, fechalanzamiento as i_fechalanzamiento, tarifaxdia as i_tarifaxdia, formatorenta as i_formatorenta, genero as i_genero, TIPOITEM_id FROM VI_ITEMS;
+
+SELECT id as ti_id, descripcion as ti_descripcion FROM VI_TIPOITEM;
+
+          SELECT i.id as i_id, i.nombre as i_nombre, i.descripcion as i_descripcion, i.fechalanzamiento as i_fechalanzamiento, i.tarifaxdia as i_tarifaxdia, i.formatorenta as i_formatorenta, i.genero as i_genero, TIPOITEM_id FROM VI_ITEMS as i;
--- tables.sql
+++ tables.sql
@@ -45,12 +45,12 @@ INSERT INTO VI_TIPOITEM (id, descripcion) VALUES
 
 -- Para la tabla VI_ITEMS
 INSERT INTO VI_ITEMS (id, nombre, descripcion, fechalanzamiento, tarifaxdia, formatorenta, genero, TIPOITEM_id) VALUES
-(1, 'Televisor', 'Televisor de 50 pulgadas', '2023-01-01', 5000, 'Diario', 'Electrodoméstico', 1),
-(2, 'Sofá', 'Sofá de tres puestos', '2022-12-01', 8000, 'Diario', 'Mueble', 2),
-(3, 'Martillo', 'Martillo de carpintero', '2023-02-15', 2000, 'Diario', 'Herramienta', 3);
+(1, 'Televisor', 'Televisor de 50 pulgadas', '2023-01-01 00:00:00.00', 5000, 'Diario', 'Electrodoméstico', 1),
+(2, 'Sofá', 'Sofá de tres puestos', '2022-12-01 00:00:00.00', 8000, 'Diario', 'Mueble', 2),
+(3, 'Martillo', 'Martillo de carpintero', '2023-02-15 00:00:00.00', 2000, 'Diario', 'Herramienta', 3);
 
 -- Para la tabla VI_ITEMRENTADO
-INSERT INTO VI_ITEMRENTADO (id,CLIENTES_documento, ITEMS_id, fechainiciorenta, fechafinrenta) VALUES
- (1,123456789, 1, '2024-03-15 00:00:00', '2024-03-20 00:00:00'),
- (2,987654321, 2, '2024-03-10 00:00:00', '2024-03-18 00:00:00'),
- (3,555555555, 3, '2024-03-12 00:00:00', '2024-03-17 00:00:00')
+INSERT INTO VI_ITEMRENTADO (id, CLIENTES_documento, ITEMS_id, fechainiciorenta, fechafinrenta) VALUES
+ (1, 123456789, 1, '2024-03-15 00:00:00.00', '2024-03-20 00:00:00.00'),
+ (2, 987654321, 2, '2024-03-10 00:00:00.00', '2024-03-18 00:00:00.00'),
+ (3, 555555555, 3, '2024-03-12 00:00:00.00', '2024-03-17 00:00:00.00');
